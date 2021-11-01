d42cb3d89be98db437806701e3f5500a3d35d244
Ash <a.schoha@gmail.com>
2021-11-01 12:42:46 +0100

    sidebar is loaded rather than static

---
+++ assets/js/load-sidebar.js
@@ -0,0 +1,39 @@
+
+function load_sidebar(url)
+{
+    fetch(url)
+        .then(function (response) 
+        {
+            return response.json();  
+        })
+        .then(function (data) 
+        {
+            var sidebar_container = document.getElementById("sidebar");
+            for (var i = 0; i < data.length; i++) 
+            {
+                var bold = document.createElement("b");
+                if (data[i]["category"] == "daily")
+                {
+                    bold.innerHTML = "D ";
+                } else {
+                    bold.innerHTML = "A ";
+                }
+
+                var span = document.createElement("span");
+                span.appendChild(bold);
+                span.innerHTML += data[i]["title"];
+
+                var link = document.createElement("a");
+                link.setAttribute("href", data[i]["url"]);
+                link.setAttribute("class", "sidebar-item grey-link");
+                link.appendChild(span);  
+                
+                sidebar_container.appendChild(link);
+            }
+        })
+        .catch(function (err) 
+        {
+            console.log("Could not fetch " + url);
+            console.log("Error: " + err);
+        });
+}
